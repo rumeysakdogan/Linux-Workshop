fd8457eaf2e556701f1489c090813fd82af42658
rumeysakdogan <dgn.rumeysaa@gmail.com>
2022-11-08 11:05:54 -0500

    adding project-104

---
+++ projects/Project-104-Bash_Script/cron_job_for_backups.sh
@@ -0,0 +1,4 @@
+# m  h  dom mon   dow command
+
+  59 23  *  DEC   SUN tar -zcf /var/backups/home.tgz /home/
+
---
+++ projects/Project-104-Bash_Script/hungry.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+echo "I am hungry. Feed me data." >> ~/Desktop/demands.txt
+date >> ~/demands.log
